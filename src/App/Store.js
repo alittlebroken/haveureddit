d414f75ec70a6fdffaa043d7425ce4e57c0842b7
Paul Lockyer <plockyer@googlemail.com>
2021-11-10 22:41:33 +0000

    Removed all hide show functionality for Post component from here

--- src/App/Store.js
+++ src/App/Store.js
@@ -1,14 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { feedReducer } from '../Features/Feed/feedSlice';
 import { modalReducer } from '../Features/Modal/ModalSlice';
-import { PostReducer } from '../Features/Post/PostSlice';
 import { togglerReducer} from '../Features/Toggler/TogglerSlice';
 
 export const Store = configureStore({
   reducer: {
     feed: feedReducer,
     modal: modalReducer,
-    post: PostReducer,
     toggler: togglerReducer,
   }
 });
--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -3,10 +3,21 @@ import React from 'react';
 
 // Create component
 const Post = (props) => {
+
   return(
     <div className="postOverlay">
       <div className="postContainer">
-
+        <div className="postHeader">
+          <button
+          className="postCloseButton"
+          onClick={props.onClick}>
+            &#10005;
+          </button>
+        </div>
+        <div><h2>{props.id}</h2></div>
+        <div>{props.parent}</div>
+        <div className="postContent"></div>
+        <div className="postComments"></div>
       </div>
     </div>
   )
