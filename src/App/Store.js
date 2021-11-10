490752c76753a12d7ed827db0396d5bfe1b13dd9
Paul Lockyer <plockyer@googlemail.com>
2021-11-10 22:37:39 +0000

    Added the toggler reducer

--- src/App/Store.js
+++ src/App/Store.js
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { feedReducer } from '../Features/Feed/feedSlice';
 import { modalReducer } from '../Features/Modal/ModalSlice';
+import { PostReducer } from '../Features/Post/PostSlice';
+import { togglerReducer} from '../Features/Toggler/TogglerSlice';
 
 export const Store = configureStore({
   reducer: {
     feed: feedReducer,
     modal: modalReducer,
+    post: PostReducer,
+    toggler: togglerReducer,
   }
 });
