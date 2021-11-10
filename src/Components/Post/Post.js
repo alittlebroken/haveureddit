841227669dba1cb94786dc096003dd79b537cf23
Paul Lockyer <plockyer@googlemail.com>
2021-11-10 11:28:36 +0000

    Created post component

---
+++ src/Components/Post/Post.js
@@ -0,0 +1,16 @@
+// Standard library imports
+import React from 'react';
+
+// Create component
+const Post = (props) => {
+  return(
+    <div className="postOverlay">
+      <div className="postContainer">
+
+      </div>
+    </div>
+  )
+};
+
+// Export the component
+export default Post;
