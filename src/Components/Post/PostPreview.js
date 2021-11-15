21b077355c05d31b68bb3ddd00bab2c58b80a10e
Paul Lockyer <plockyer@googlemail.com>
2021-11-15 15:06:56 +0000

    Added informational buttons to the post preview component

--- src/Components/Post/PostPreview.js
+++ src/Components/Post/PostPreview.js
@@ -9,6 +9,8 @@ import Post from './Post'
 import Toggler from '../Toggler/Toggler';
 import { toggleOnOff } from '../../Features/Toggler/TogglerSlice'
 
+// Utility components
+import { abbreviateNumber } from '../../utilities.js';
 // Component Stylesheet
 import './postpreview.css';
 
@@ -69,6 +71,10 @@ const PostPreview = (props) => {
       document.body.setAttribute('style', `position: fixed; top: -${windowOffset}px; left: 0; right: 0;`);
     };
 
+    // Format information for numerical data like awards and upvotes
+    let upVotes = abbreviateNumber(props.post.ups);
+    let awards = abbreviateNumber(props.post.total_awards_received);
+
   return (
     <div>
     <Toggler id={props.post.id}>
@@ -83,25 +89,57 @@ const PostPreview = (props) => {
         handleOpen({id:props.post.id, state: true})
       }}>
         <div>
+
           <h4>{postTitle}</h4>
-          <span className="post-author">Posted by: {postAuthor}</span>
+
+          <span className="post-author">
+          Posted by: {postAuthor}
+          </span>
+
         </div>
+
         <div className="post-center-media">
           {renderMedia}
         </div>
+
+        <span className="button-info-container">
+
+          <button className="post-preview-button-info">
+            <i
+            className="far fa-comment"
+            title="comments" alt="comments"
+            ></i>
+            &nbsp;{postComments}
+          </button>
+
+          <button className="post-preview-button-info">
+            <i
+            className="fas fa-angle-double-up"
+            title="upVote" alt="upVote"></i>
+            &nbsp;{upVotes}&nbsp;
+            <i
+            className="fas fa-angle-double-down"
+            title="downVote" alt="downVote"></i>
+          </button>
+
+          <button className="post-preview-button-info">
+            <i className="fas fa-trophy"
+            title="Awards" alt="Awards"></i>
+            &nbsp;{awards}
+          </button>
+
+        </span>
+
       </div>
+
       <div className="post-info">
         <div>
-          <span className="post-subreddit">From: <button className="button-link" onClick={handleSetNameClick}>r/{postSubReddit}</button></span>
-        </div>
-        <div>
-          <span
-          className="post-comment-count">
-          <i
-          class="far fa-comment"
-          title="comments" alt="comments"
-          ></i>
-          &nbsp;{postComments}
+          <span className="post-subreddit">
+            <button
+              className="button-link"
+              onClick={handleSetNameClick}>
+              r/{postSubReddit}
+            </button>
           </span>
         </div>
       </div>
