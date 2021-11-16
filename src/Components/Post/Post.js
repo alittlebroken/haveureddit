9428cdcaabd857be40e007209f74572d2aa03415
Paul Lockyer <plockyer@googlemail.com>
2021-11-16 12:10:06 +0000

    Fixed issue with image reddit galleries not displaying

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -18,21 +18,38 @@ const Post = (props) => {
     over_18
   } = data;
 
-  console.log(`Post Text: ${data.selftext}`)
+  // Work out if post is image, video,
+  const videoPost = data.is_video;
+
+  const textPost = data.is_self;
+
+  const galleryPost = data.is_gallery;
+
+  let imagePost = false;
+  if(data.domain === 'i.redd.it' || data.domain === 'i.imgur.com'){
+    imagePost = true;
+  }
+
+
+
+  let externalPost = false;
+  if(data.domain !== 'i.redd.it' || data.domain !== 'v.redd.it' || data.domain !== 'i.imgur.com'|| data.domain !== 'reddit.com'){
+    externalPost = true;
+  }
 
   // get the media from the page
   let media;
-  if(data.is_video){
+  if(videoPost){
     // Video
     const media_src = data.media.reddit_video.fallback_url;
     media = <div className="postContent">
-    <video width="100%" height="80%" controls autoplay muted>
+    <video width="100%" height="80%" controls autoPlay muted>
       <source src={media_src} />
       Your browser does not support the video tag
     </video>
     </div>;
 
-  } else if(data.is_self){
+  } else if(textPost){
     // Text post
     media = <div
     className={data.is_self ? "postContent postContentText": "postContent"}>
@@ -46,14 +63,44 @@ const Post = (props) => {
       media = null;
     }
 
-  } else if (!data.is_self && !data.is_video){
-    // Check if just an image only
-    if(data.media === null && !data.media_embed.length){
+  } else if (imagePost){
+
       let imgSrc = data.url;
       media = <div className="postContent">
         <img src={imgSrc} width="100%" alt={title}/>
       </div>;
-    }
+
+  } else if(galleryPost){
+
+    /* Extract the ids and then extract the images associated with those
+     ids */
+    media = data.gallery_data.items.map(item => {
+
+      let id = item.media_id;
+      let imgUrl = data.media_metadata[id].s.u;
+
+      // Extract the image name from the url
+      let imgPattern = /\w*\.\w{3}(?=\?)/g;
+      let imgSrc = imgUrl.match(imgPattern);
+      // Pass in the image name to the reddit image server
+      let finalUrl = `https://i.redd.it/${imgSrc[0]}`;
+
+      return <img
+      src={finalUrl}
+      className="postImgGallery" />
+
+    });
+
+  } else if(externalPost){
+    media = <div className="postExternalLink"><h2>Warning</h2><p>
+    The link below is to an external site of which we have no control over. <br /><br />
+    As such we can not guarantee the content that will greet you once the link
+    has been clicked.<br /><br />
+    Proceed at your own risk.
+    </p>
+    <br />
+    <a href={data.url} target="_blank" rel="noreferrer">Link</a>
+    </div>;
   }
 
   // Do we have a nsfw post?
