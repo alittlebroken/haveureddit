69cf0ae84f2eb61868dada789c12fa8c9e0bd255
Paul Lockyer <plockyer@googlemail.com>
2021-11-18 21:59:53 +0000

    Fixed typos with package imports and exporting the reducer

--- src/Features/subReddits/subRedditsSlice.js
+++ src/Features/subReddits/subRedditsSlice.js
@@ -1,17 +1,36 @@
 // Import packages and stylesheets
-import { createSlice } from '@reducjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
 // Options for the slice
 const options = {
   name: 'subreddits',
-  initialState: {},
-  reducers: {},
+  initialState: {
+    subreddits: [
+      {name: 'popular', sort: 'hot'},
+      {name: 'news', sort: 'hot'},
+      {name: 'MostBeautiful', sort: 'hot'},
+      {name: 'nasa', sort: 'hot'},
+      {name: 'pics', sort: 'hot'},
+      {name: 'technology', sort: 'hot'},
+      {name: 'ProgrammerHumor', sort: 'hot'},
+      {name: 'coding', sort: 'hot'},
+      {name: 'science', sort: 'hot'},
+      {name: 'IAmA', sort: 'hot'},
+    ]
+  },
+  reducers: {
+    addSubReddit: (state, action) => {
+      state.subreddits.push(action.payload);
+    }
+  },
 };
 
 // create the slice
 const subRedditsSlice = createSlice(options);
 
 // create and export the selectors for the slice
+export const selectSubReddits = state => state.subReddits.subreddits;
 
 // export actions and reducer for the slice
-export default subRedditsReducer = subRedditsSlice.reducer;
+const { addSubReddit } = subRedditsSlice.actions;
+export const subRedditsReducer = subRedditsSlice.reducer;
