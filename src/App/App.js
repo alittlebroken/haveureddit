6915d5993c6ce566ba7c22d5da76767a6a662514
Paul Lockyer <plockyer@googlemail.com>
2021-11-18 21:45:14 +0000

    Added the SubReddits component

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Feed from '../Components/Feed/Feed';
 import SearchBar from '../Components/SearchBar/SearchBar';
+import SubReddits from '../Components/subReddits/subReddits';
 
 function App() {
   return (
     <div>
       <SearchBar />
+      <SubReddits />
       <Feed />
     </div>
   );
