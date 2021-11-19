d89c936f264381c2ac1bbdb731c6dadda9d4bf57
Paul Lockyer <plockyer@googlemail.com>
2021-11-19 11:13:01 +0000

    Added styling to the application container

--- src/App/App.js
+++ src/App/App.js
@@ -3,9 +3,11 @@ import Feed from '../Components/Feed/Feed';
 import SearchBar from '../Components/SearchBar/SearchBar';
 import SubReddits from '../Components/subReddits/subReddits';
 
+import './app.css';
+
 function App() {
   return (
-    <div>
+    <div className="appContainer">
       <SearchBar />
       <SubReddits />
       <Feed />
