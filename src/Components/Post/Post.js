7bcd512b7f08ff583fc49d6558f984e9afdeecaa
Paul Lockyer <plockyer@googlemail.com>
2021-11-25 11:59:30 +0000

    Added ability to load initial comments for a post

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -1,12 +1,21 @@
 // Standard library imports
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactMarkdown from 'react-markdown';
+import { useSelector, useDispatch } from 'react-redux';
+import CommentsList from '../Comments/CommentsList.js';
+import {
+  selectComments,
+  loadComments,
+  selectIsLoading,
+  selectHasError } from '../../Features/Comments/commentsSlice.js';
 
 // Stylesheet
 import './post.css';
 
 // Create component
 const Post = (props) => {
+
+  const dispatch = useDispatch();
 
   // Gather the relevant information from the props
   const data = props.content;
@@ -15,106 +24,143 @@ const Post = (props) => {
     author,
     subreddit,
     num_comments,
-    over_18
+    over_18,
+    id
   } = data;
 
-  // Work out if post is image, video,
-  const videoPost = data.is_video;
+  // load the initial comments
+  useEffect(() => {
+    dispatch(loadComments({
+      name: subreddit,
+      id: id,
+    }))
+  },[]);
 
-  const textPost = data.is_self;
+  // Get list of comments
+  const comments = useSelector(selectComments);
+  const isLoading = useSelector(selectIsLoading);
+  const hasError = useSelector(selectHasError);
 
-  const galleryPost = data.is_gallery;
-
-  let imagePost = false;
-  if(data.domain === 'i.redd.it' || data.domain === 'i.imgur.com'){
-    imagePost = true;
-  }
-
-
-
-  let externalPost = false;
-  if(data.domain !== 'i.redd.it' || data.domain !== 'v.redd.it' || data.domain !== 'i.imgur.com'|| data.domain !== 'reddit.com'){
-    externalPost = true;
-  }
-
-  // get the media from the page
+  // Various vars for the post
+  let nsfw;
   let media;
-  if(videoPost){
-    // Video
-    const media_src = data.media.reddit_video.fallback_url;
-    media = <div className="postContent">
-    <video width="100%" height="80%" controls autoPlay muted>
-      <source src={media_src} />
-      Your browser does not support the video tag
-    </video>
-    </div>;
+  let imagePost = false;
+  let externalPost = false;
 
-  } else if(textPost){
-    // Text post
+  if(isLoading){
     media = <div
-    className={data.is_self ? "postContent postContentText": "postContent"}>
-    <pre><ReactMarkdown>
-      {data.selftext}
-    </ReactMarkdown></pre>
+    id="loading"
+    className="loading">
+      <div className="loadingContent">
+        <img
+        src="/loading_001.svg"
+        title="loading content"
+        alt="loading content" />
+        <h2 className="loadingHeader">
+          Loading please wait...
+        </h2>
+      </div>
     </div>;
+  } else if(hasError){
 
-    // Do not display the element if no text has been entered
-    if(!data.selftext){
-      media = null;
+  } else {
+    // Work out if post is image, video,
+    const videoPost = data.is_video;
+    const textPost = data.is_self;
+    const galleryPost = data.is_gallery;
+
+    if(data.domain === 'i.redd.it' || data.domain === 'i.imgur.com'){
+      imagePost = true;
     }
 
-  } else if (imagePost){
+    if(data.domain !== 'i.redd.it' || data.domain !== 'v.redd.it' || data.domain !== 'i.imgur.com'|| data.domain !== 'reddit.com'){
+      externalPost = true;
+    }
 
-      let imgSrc = data.url;
+    // get the media from the page
+    if(videoPost){
+      // Video
+      const media_src = data.media.reddit_video.fallback_url;
       media = <div className="postContent">
-        <img src={imgSrc} width="100%" alt={title}/>
+      <video width="100%" height="80%" controls autoPlay muted>
+        <source src={media_src} />
+        Your browser does not support the video tag
+      </video>
       </div>;
 
-  } else if(galleryPost){
+    } else if(textPost){
+      // Text post
+      media = <div
+      className={data.is_self ? "postContent postContentText": "postContent"}>
+      <pre><ReactMarkdown>
+        {data.selftext}
+      </ReactMarkdown></pre>
+      </div>;
 
-    /* Extract the ids and then extract the images associated with those
-     ids */
-    media = data.gallery_data.items.map(item => {
+      // Do not display the element if no text has been entered
+      if(!data.selftext){
+        media = null;
+      }
 
-      let id = item.media_id;
-      let imgUrl = data.media_metadata[id].s.u;
+    } else if (imagePost){
 
-      // Extract the image name from the url
-      let imgPattern = /\w*\.\w{3}(?=\?)/g;
-      let imgSrc = imgUrl.match(imgPattern);
-      // Pass in the image name to the reddit image server
-      let finalUrl = `https://i.redd.it/${imgSrc[0]}`;
+        let imgSrc = data.url;
+        media = <div className="postContent">
+          <img src={imgSrc} width="100%" alt={title}/>
+        </div>;
 
-      return <img
-      src={finalUrl}
-      className="postImgGallery" />
+    } else if(galleryPost){
 
-    });
+      /* Extract the ids and then extract the images associated with those
+       ids */
+      media = data.gallery_data.items.map(item => {
 
-  } else if(externalPost){
-    media = <div className="postExternalLink"><h2>Warning</h2><p>
-    The link below is to an external site of which we have no control over. <br /><br />
-    As such we can not guarantee the content that will greet you once the link
-    has been clicked.<br /><br />
-    Proceed at your own risk.
-    </p>
-    <br />
-    <a href={data.url} target="_blank" rel="noreferrer">Link</a>
+        let id = item.media_id;
+        let imgUrl = data.media_metadata[id].s.u;
+
+        // Extract the image name from the url
+        let imgPattern = /\w*\.\w{3}(?=\?)/g;
+        let imgSrc = imgUrl.match(imgPattern);
+        // Pass in the image name to the reddit image server
+        let finalUrl = `https://i.redd.it/${imgSrc[0]}`;
+
+        return <img
+        src={finalUrl}
+        className="postImgGallery" />
+
+      });
+
+    } else if(externalPost){
+      media = <div className="postExternalLink"><h2>Warning</h2><p>
+      The link below is to an external site of which we have no control over. <br /><br />
+      As such we can not guarantee the content that will greet you once the link
+      has been clicked.<br /><br />
+      Proceed at your own risk.
+      </p>
+      <br />
+      <a href={data.url} target="_blank" rel="noreferrer">Link</a>
+      </div>;
+    }
+
+    // Do we have a nsfw post?
+    if(over_18){
+      nsfw = <i
+      class="fas fa-exclamation-triangle"
+      alt="Over 18s only (NSFW)"
+      title="Not Safe for Work ( 18+)"></i>;
+    } else {
+      nsfw = null;
+    }
+  }
+
+  /* Only display comments if we have any to display */
+  let showComments = null;
+  if(comments.length > 0) {
+    showComments = <div className="postSubInfo">
+      <CommentsList comments={comments}/>
     </div>;
   }
 
-  // Do we have a nsfw post?
-  let nsfw;
-  if(over_18){
-    nsfw = <i
-    class="fas fa-exclamation-triangle"
-    alt="Over 18s only (NSFW)"
-    title="Not Safe for Work ( 18+)"></i>;
-  } else {
-    nsfw = null;
-  }
-
-  console.log(data)
 
   return(
 
@@ -145,13 +191,7 @@ const Post = (props) => {
 
           {media}
 
-          <div className="postSubInfo">
-            <i
-            class="far fa-comment"
-            title="comments" alt="comments"
-            ></i>
-            &nbsp;{num_comments}
-          </div>
+          {showComments}
 
         </div>
 
